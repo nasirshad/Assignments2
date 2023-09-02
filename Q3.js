96c6ed8f9c9cc0fd4d250b997a3f59af1005b64d
nasirshad <muhammadnasirshad96@gmail.com>
2023-09-02 10:46:12 +0500

    Message

---
+++ Q3.js
@@ -0,0 +1,12 @@
+"use strict";
+// Q 3:  Write a program that checks if a given number is 
+// positive, negative, or zero.
+var num = 0;
+if (num > 0) {
+    console.log("The given number is Positive");
+}
+else if (num < 0) {
+    console.log("The given number is Negative");
+}
+else
+    console.log("The given number is Zero");
---
+++ Q3.ts
@@ -0,0 +1,12 @@
+// Q 3:  Write a program that checks if a given number is 
+// positive, negative, or zero.
+var num:number=0
+if (num>0) 
+{
+    console.log("The given number is Positive")
+} 
+else if (num<0)
+{
+    console.log("The given number is Negative")
+}
+else console.log("The given number is Zero");
