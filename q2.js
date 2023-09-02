05097c0eadf83abe8bc0f262bd12a281598c5d85
nasirshad <muhammadnasirshad96@gmail.com>
2023-09-02 10:36:34 +0500

    Message

---
+++ q2.js
@@ -0,0 +1,8 @@
+"use strict";
+// Q 2: Write a program that takes input and calculates the volume 
+// of a cube.
+var height = 20;
+var width = 30;
+var length = 400;
+var area = height * length * width;
+console.log(area);
---
+++ q2.ts
@@ -0,0 +1,7 @@
+// Q 2: Write a program that takes input and calculates the volume 
+// of a cube.
+var height:number=20;
+var width:number=30;
+var length:number=400;
+var area:number=height*length*width
+console.log(area)
